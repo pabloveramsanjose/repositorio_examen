5081f8068c1c7f3b2a5207a2e11f2ed828f5900e
pabloveramsanjose <pabloveram.sanjose@fundacionloyola.net>
2026-03-06 12:57:38 +0100

    Comitt 2 rama CA

---
+++ EXAMEN_ENDE/src/exa2ende2526/enunciado/CuentaAhorro.java
@@ -0,0 +1,16 @@
+package exa2ende2526.enunciado;
+
+public class CuentaAhorro {
+	
+	public CuentaAhorro(double saldoInicial,String titular) {
+		super();
+		int comision;
+		
+	}
+
+	public void calcularInteres(double saldoInicial, int comision) {
+		
+		double interes = saldoInicial * comision;
+		
+	}
+}
