0072f7f4e8708c4d9b08f5409925c79f8d91421d
pabloveramsanjose <pabloveram.sanjose@fundacionloyola.net>
2026-03-06 13:01:42 +0100

    Commit 2 CC

---
+++ EXAMEN_ENDE/src/exa2ende2526/enunciado/CuentaCorriente.java
@@ -0,0 +1,15 @@
+package exa2ende2526.enunciado;
+
+public class CuentaCorriente {
+	
+	public CuentaCorriente(double saldoInicial,String titular,int comision) {
+		super();
+		
+	}
+
+	public void calcularInteres(double saldoInicial, int comision) {
+		
+		double interes = comision;
+		
+	}
+}
